1330372e355bef2ed17558f802f70ac633b253b1
Parth Dagia <parth.24bcs10414@sst.scaler.com>
2026-05-30 21:00:32 +0530

    infra: bootstrap springboot project
    Signed-off-by: Parth Dagia <parth.24bcs10414@sst.scaler.com>

---
+++ src/test/java/com/bigo/BigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
